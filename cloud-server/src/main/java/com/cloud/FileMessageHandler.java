f3752a3da848f1a849521d94d69d94555c65c140
VladislavChernyshev <v.chernyshev911@gmail.com>
2023-01-30 18:07:39 +1000

    Сделан простой интерфейс и сделан простой сервер для отображения списка файлов на клиенте.

---
+++ cloud-server/src/main/java/com/cloud/FileMessageHandler.java
@@ -0,0 +1,38 @@
+package com.cloud;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+
+public class FileMessageHandler implements Runnable {
+
+    private final File dir;
+    private final DataInputStream is;
+    private final DataOutputStream os;
+
+    public FileMessageHandler(Socket socket) throws IOException {
+        is = new DataInputStream(socket.getInputStream());
+        os = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Клиент подключился");
+        dir = new File("files");
+        String[] files = dir.list();
+        os.writeUTF("#list#");
+        os.writeLong(files.length);
+        for (String file : files) {
+            os.writeUTF(file);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                is.readUTF();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
